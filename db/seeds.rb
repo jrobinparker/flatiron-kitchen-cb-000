1968bc34a0025e0d24fcc8945be720f0b4426526
Jeremy Parker <jrobinparker@gmail.com>
2018-07-05 20:54:40 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # Trisha Yearwood's Blueberry Pancakes Recipe
 # http://www.foodnetwork.com/recipes/trisha-yearwood/blueberry-pancakes-recipe.html
 
-blueberry_pancakes = Recipe.new(name: 'Blueberry Pancakes')
+blueberry_pancakes = Recipe.new(recipe_name: 'Blueberry Pancakes')
 blueberry_pancakes_ingredients = ['All-purpose flour', 'Sugar', 'Baking powder', 'Baking soda', 'Salt', 'Large eggs', 'Milk', 'Sour cream', 'Butter', 'Vanilla extract', 'Blueberries', 'Lemon zest']
-blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(name: i)}
+blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(ingredients_name: i)}
 blueberry_pancakes.save
 
 # Jake the Dog's Bacon Pancakes Recipe
